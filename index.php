d935a65ad5eee9e0d5b16e64bbc74cbb4ca672a7
Dima Korobka <korobka_dv@ukr.net>
2018-03-29 05:49:25 +0300

    Hou HouHou

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+<?php
+
+$test = "Hello";
+$name = "test";
+echo "$$name";
+
+?>
+	</body>
+</html>
+
