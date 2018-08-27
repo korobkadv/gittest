e36cf5813f941a2f1a6c5799dbaafae0647033f9
Dima Korobka <korobka_dv@ukr.net>
2018-08-28 01:14:29 +0300

    var

--- js-module-01/js/scripts-dop.js
+++ js-module-01/js/scripts-dop.js
@@ -4,12 +4,14 @@ const taba = 6;
 
 let go;
 let isComing;
+let result;
 
 const userNumberIn = prompt('Сколько мест необходимо?');
 let userNumber = Number(userNumberIn);
 
  if (userNumber == "0") {
      alert("Очень жаль, приходите еще!");
+     result = "Очень жаль, приходите еще!";
  }
  else if (!Number.isNaN(userNumber)){
 
@@ -22,6 +24,7 @@ let userNumber = Number(userNumberIn);
      if (isComing) {
         alert("Приятного путешествия в группе sharm");
         go = "sharm";
+        result = "Приятного путешествия в группе sharm";
      }
    }
    if (userNumber <= hurgada && go != "sharm") {
@@ -29,6 +32,7 @@ let userNumber = Number(userNumberIn);
      if (isComing) {
         alert("Приятного путешествия в группе hurgada");
         go = "hurgada";
+        result = "Приятного путешествия в группе hurgada";
      }
    }
    if (userNumber <= taba && go != "sharm" && go != "hurgada") {
@@ -36,19 +40,23 @@ let userNumber = Number(userNumberIn);
      if (isComing) {
         alert("Приятного путешествия в группе taba");
         go = "taba";
+        result = "Приятного путешествия в группе taba";
      }
    }
  
     if (!isComing) {
       alert("Нам очень жаль, приходите еще!");
+      result = "Нам очень жаль, приходите еще!";
     }
     
   } else { 
     alert("Извините, столько мест нет ни в одной группе!");
+    result = "Извините, столько мест нет ни в одной группе!";
     }
  
  }
  else {
     alert("Ошибка ввода!");
+    result = "Ошибка ввода!";
   }
 
