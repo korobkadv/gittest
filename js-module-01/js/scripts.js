f3b446d8e3fa2bb625a5f5e9ba5b4794fd63314d
Dima Korobka <korobka_dv@ukr.net>
2018-08-30 20:17:14 +0300

    comment JS module 01

--- js-module-01/js/scripts.js
+++ js-module-01/js/scripts.js
@@ -7,25 +7,25 @@ const cancel = 'Отменено пользователем!';
 const hello = 'Добро пожаловать!';
 const stop = 'Доступ запрещен!';
 
- if (!userLogin) {
-    alert(cancel);
-    result = cancel;
+ if (!userLogin) {    // Пользователь нажал "cancel"
+    alert(cancel);    // Выводим "Отменено пользователем!"
+    result = cancel;  // Заносим результат в переменную для возможного, дальнейшего использования
  }
- else if (userLogin === adminLogin) {
-    const userPassword = prompt('Введите пароль:');
-        if (!userPassword) {
-            alert(cancel); 
-            result = cancel; 
+ else if (userLogin === adminLogin) { // Если пользователь ввел правильный логин
+    const userPassword = prompt('Введите пароль:'); // Спрашиваем пароль
+        if (!userPassword) {  // Пользователь нажал "cancel"
+            alert(cancel);    // Выводим "Отменено пользователем!"
+            result = cancel;  
         }
-        else if (userPassword === adminPassword) {
-            alert(hello);
+        else if (userPassword === adminPassword) { // Если пользователь ввел правильный пароль
+            alert(hello);                          // Выводим 'Добро пожаловать!'
             result = hello;
-        } else {
-            alert(stop);
+        } else {                                   // Если пользователь ввел НЕ правильный пароль
+            alert(stop);                           // Выводим 'Доступ запрещен!'
             result = stop;
         }
  } 
- else {
-    alert(stop);
+ else {               // Если пользователь ввел НЕ правильный логин
+    alert(stop);      // Выводим 'Доступ запрещен!'
     result = stop;
  }
