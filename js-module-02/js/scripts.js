e322892b1f99e790a84aa806b22d193caec87ae4
Dima Korobka <korobka_dv@ukr.net>
2018-09-02 13:20:42 +0300

    comments JS module 02

--- js-module-02/js/scripts.js
+++ js-module-02/js/scripts.js
@@ -3,20 +3,20 @@ const numbers = [];
 let total = 0;
 
 do {
-    userInput = Number( prompt('Введите число', '') );
-      if (!userInput) { break; }
-      numbers.push(userInput);
+    userInput = Number( prompt('Введите число', '') ); // Просим ввести число
+      if (!userInput) { break; } // Если нажали "Cancel" то тормозим
+      numbers.push(userInput);   // Вводим число в конец массива
   
-} while(!isNaN(userInput));
+} while(!isNaN(userInput)); // Продолжаем цикл пока не нажмется "Cancel"
 
-if (numbers.length !== 0) {
+if (numbers.length !== 0) { 
    
-    for(const value of numbers) {
+    for(const value of numbers) { // Считаем общую сумму массива
         total += value;
     } 
-    alert(`Общая сумма равна : ${total}`); 
+    alert(`Общая сумма равна : ${total}`); // Вводим общую сумму массива
 }
 else {
-  alert("Нужно ввести числа!");
+  alert("Нужно ввести числа!"); // Если пользователь вводит НЕ числа
 }
 
