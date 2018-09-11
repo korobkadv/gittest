0df030550c1cc399ddae0559aa947757514f67d2
Dima Korobka <korobka_dv@ukr.net>
2018-09-12 01:41:44 +0300

    JS module 3 | name variable

--- js-module-03/js/scripts.js
+++ js-module-03/js/scripts.js
@@ -1,8 +1,8 @@
 const allLogins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 
-const error = 'Ошибка! Логин должен быть от 4 до 16 символов';
-const errorTwo = 'Такой логин уже используется!';
-const proffit = 'Логин успешно добавлен!';
+const lengthError = 'Ошибка! Логин должен быть от 4 до 16 символов';
+const existanceError = 'Такой логин уже используется!';
+const successMessage = 'Логин успешно добавлен!';
 
 const isLoginValid = login => login.length >= 4 && login.length <=16;
 const isLoginUnique = login => allLogins.includes(login);
@@ -10,14 +10,14 @@ const isLoginUnique = login => allLogins.includes(login);
 const addLogin = function(login) {
  
   if (!isLoginValid(login)) { 
-    console.log(error);  
+    console.log(lengthError);  
    }
   if (isLoginUnique(login)) {
-    console.log(errorTwo);  
+    console.log(existanceError);  
    }
   if (!isLoginUnique(login) && isLoginValid(login)) { 
     allLogins.push(login);
-    console.log(proffit);    
+    console.log(successMessage);    
    }
 
 };
